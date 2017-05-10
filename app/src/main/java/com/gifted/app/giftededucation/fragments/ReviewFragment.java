c281a311d6cc91bd2fed53b09b00b6788f433ca0
yagyak <yagyak@tnine.io>
2017-05-10 15:26:07 +0530

    all done

--- app/src/main/java/com/gifted/app/giftededucation/fragments/ReviewFragment.java
+++ app/src/main/java/com/gifted/app/giftededucation/fragments/ReviewFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.gifted.app.giftededucation.R;
 import com.gifted.app.giftededucation.adapters.OptionsAdapter;
@@ -22,16 +23,21 @@ public class ReviewFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         OptionsAdapter optionsAdapter;
+        TextView name_user, user_class;
         View rootView = inflater.inflate(R.layout.fragment_review, container, false);
 
         ViewPager vp = (ViewPager) getActivity().findViewById(R.id.viewpager);
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.review_recycler);
+        name_user = (TextView) rootView.findViewById(R.id.name_user);
+        user_class = (TextView) rootView.findViewById(R.id.user_class);
 
         optionsAdapter = new OptionsAdapter(vp, getActivity());
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(Base.getContext(), 5);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(optionsAdapter);
+        name_user.setText("Abhishek Dubey");
+        user_class.setText("Class : 8");
 
 
         return rootView;
